e934a466ecc0012fcefdd23fb9702bc8e58d216e
web3Codess <vercelploy@gmail.com>
2022-08-07 17:49:22 +0300

    commit on 'origin'

--- pages/index.js
+++ pages/index.js
@@ -103,7 +103,7 @@ const Home = () => {
     <div className="flex justify-center sm:px-4 p-12">
       <div className="w-full minmd:w-4/5">
         <Banner
-          name={(<>Discoveee, collect, and sell <br /> extraordinary NFTs</>)}
+          name={(<>Discoveee, collect, and sell0 <br /> extraordinary NFTs</>)}
           childStyles="md:text-4xl sm:text-2xl xs:text-xl text-left"
           parentStyle="justify-start mb-7 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
         />
@@ -113,7 +113,7 @@ const Home = () => {
         ) : isLoading ? <Loader /> : (
           <>
             <div>
-              <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 xs:ml-0">Best Creators</h1>
+              <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 xs:ml-0">Best Creatorss</h1>
 
               <div className="relative flex-1 max-w-full flex mt-3" ref={parentRef}>
                 <div className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none" ref={scrollRef}>
